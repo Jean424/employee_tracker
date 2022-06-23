a1661c34dd7021beb53149719539a27d115a0d0d
Jean Lee <jeanlee.jingli@icloud.com>
2022-06-23 14:00:54 -0700

    add database and tables and seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,12 @@
 INSERT INTO department (name)
-VALUES ("Web Development"),
-       ("Data Science"),
-       ("Math"),
-       ("Electives");
+VALUES ("IT"),
+       ("Accouting");
 
-INSERT INTO course_names (name, department)
-VALUES ("Intro to JavaScript", 1),
-       ("Data Science", 2),
-       ("Linear Algebra", 3),
-       ("History of the Internet", 4),
-       ("Machine Learning", 4),
-       ("Game Design", 1 ),
-       ("Cloud Development", 1);
-       
+INSERT INTO role (title, salary, department_id)
+VALUES ("IT manager", 7255.00, 1),
+       ("Accounting manager", 5860.00, 2);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("David", "Lee", 1, NULL),
+       ("Sue", "Su", 2, NULL);    
