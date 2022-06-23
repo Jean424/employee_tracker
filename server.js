e9832d937f59b9b7c0beb8c29bf33757a6df8c38
Jean Lee <jeanlee.jingli@icloud.com>
2022-06-23 14:54:08 -0700

    add question function in server.js

--- server.js
+++ server.js
@@ -1,35 +1,84 @@
-const express = require('express');
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+const consoleTable = require('console.table')
 
 const db = mysql.createConnection(
   {
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'books_db'
+    database: 'employee_tracker_db'
   },
-  console.log(`Connected to the books_db database.`)
+  console.log(`Connected to the employee_tracker_db database.`)
 );
 
+function menu() {
+  // TODO: Create an array of questions for user input
+inquirer
+.prompt([
+      {
+          type: 'list',
+          name: 'action',
+          message: 'What would you like to do?',
+          choices: [
+            'view all departments',
+            'view all roles',
+            'view all employees',
+            'add a department',
+            'add a employee',
+            'add a role',
+            'update employee',
+            'done'
+          ]
+      },
+      
+  ]).then((data) => {
+    // console.log(data.action)
+    switch (data.action) {
+      case 'view all departments':
+        viewAllDep();
+        
+        break;
+    
+      default:
+        break;
+    }
 
-db.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', function (err, results) {
-  console.log(results);
-});
+  });
 
-db.query('SELECT SUM(quantity) AS total_in_section, MAX(quantity) AS max_quantity, MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity FROM favorite_books GROUP BY section', function (err, results) {
-  console.log(results);
-});
+}
 
-app.use((req, res) => {
-  res.status(404).end();
-});
+function viewAllDep() {
+  db.query('SELECT * FROM department', function (err, results) {
+    console.table(results);
+    menu();
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+function viewAllRoles() {
+
+}
+
+function viewAllEmp() {
+
+}
+
+function addDep() {
+
+
+}
+
+function addRole() {
+
+}
+
+function addEmp() {
+
+}
+
+function updateEmp() {
+
+}
+
+menu();
+
