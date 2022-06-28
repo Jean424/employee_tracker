072bd65f98d9908f0c45103619bddd7d7beaa007
Jean Lee <jeanlee.jingli@icloud.com>
2022-06-27 21:13:21 -0700

    delete README.md, query.sql and modify schema.sql and seed.sql

--- db/schema.sql
+++ db/schema.sql
@@ -5,30 +5,29 @@ USE employee_tracker_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
+  title VARCHAR(30),
   salary DECIMAL,
-  department_id INT,
+  department_id INT NOT NULL,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
-  ON DELETE SET NULL
+  ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT,
-  manager_id INT,
+  role_id INT NOT NULL,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE CASCADE
   FOREIGN KEY (manager_id)
   REFERENCES employee(id)
   ON DELETE SET NULL,
-  FOREIGN KEY (role_id)
-  REFERENCES role(id)
-  ON DELETE SET NULL
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,22 @@
 INSERT INTO department (name)
 VALUES ("IT"),
-       ("Accouting");
+       ("Accouting")
+       ("Engineering");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("IT manager", 7255.00, 1),
-       ("Accounting manager", 5860.00, 2);
+VALUES  ("IT manager", 7255.00, 1),
+        ("ITperson", 6255.00, 1),
+        ("Accounting manager", 5860.00, 2);
+        ('Accountant', 5000.00, 2),
+        ('Lead Engineer', 10000.00, 2),
+        ('Software Engineer', 80000.00, 2),
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("David", "Lee", 1, NULL),
-       ("Sue", "Su", 2, NULL);    
+VALUES  ("David", "Lee", 1, NULL),
+        ("Sue", "Su", 2, NULL);
+        ('Ashley', 'Lu', 3, NULL),
+        ('Kevin', 'Tupik', 4, 3),
+        ('Kunal', 'Singh', 5, NULL),
+        ('Malia', 'Brown', 6, 5),
+
